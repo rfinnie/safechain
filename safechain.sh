a6d432d841532989b4ba849a218db7107c726137
Ryan Finnie <ryan@finnie.org>
2020-06-03 10:44:04 -0700

    Relicense to LGPL v3
    Approved by Canonical Ltd., uRT#35278, 2020-06-03

--- safechain.sh
+++ safechain.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 ########################################################################
-# Copyright (C) 2013 Canonical Ltd.
-# Original author: Ryan Finnie <ryan.finnie@canonical.com>
+# Copyright (C) 2013-2020 Canonical Ltd., Ryan Finnie
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
 #
 # Usage (assuming subchain.sh):
 #
