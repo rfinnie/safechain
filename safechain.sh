c2512b758c0af938e2d321693b2e354404d09e39
Ryan Finnie <ryan@finnie.org>
2020-06-03 17:25:01 -0700

    Add header info

--- safechain.sh
+++ safechain.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ########################################################################
+# Safechain idempotent firewall wrapper
 # Copyright (C) 2013-2020 Canonical Ltd., Ryan Finnie
+# https://github.com/rfinnie/safechain
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as
